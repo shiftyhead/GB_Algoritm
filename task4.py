45c43cde87c59c620643470b75a7924b92098267
shiftyhead <spirin.md@gmail.com>
2018-10-27 11:10:13 +0400

    4. Написать программу, которая генерирует в указанных пользователем границах:
        случайное целое число;
        случайное вещественное число;
        случайный символ.

---
+++ task4.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+i1 = int(input('вывести целое лисло с:\n'))
+i2 = int(input('по\n'))
+
+print('случайное целое: ', random.randint(i1, i2))
+
+f1 = float(input('вывести вещественное лисло с:\n'))
+f2 = float(input('по\n'))
+
+print('случайное вещественное: ', random.uniform(f1, f2))
+
+s1 = input('вывести символ с:\n').lower()
+s2 = input('по\n').lower()
+si1 = string.printable.find(s1)
+si2 = string.printable.find(s2)
+sr = string.printable[random.randint(si1, si2)]
+
+print('случайный символ: ', sr)
