438b72deb671b400801c3e73d97be8c345994c26
shiftyhead <spirin.md@gmail.com>
2018-11-03 17:30:49 +0400

    5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.

---
+++ task3_5.py
@@ -0,0 +1,19 @@
+"""
+5. В массиве найти максимальный отрицательный элемент.
+Вывести на экран его значение и позицию в массиве.
+"""
+
+import random
+
+SIZE = 10
+source = [random.randint(-1 * SIZE, SIZE) for _ in range(0, SIZE)]
+
+max_ = -1 * SIZE
+max_id = 0
+for id, val in enumerate(source):
+    if val < 0 and val > max_:
+        max_ = val
+        max_id = id
+
+print(f'в массиве {source}')
+print(f'максимальное отрицательное число: {max_} (позиция {max_id})')
