2f82f543a7377b7f938dd7037db6e0829f21c5b9
shiftyhead <spirin.md@gmail.com>
2018-11-19 14:10:41 +0400

    1. Отсортировать по убыванию методом «пузырька» одномерный целочисленный массив, заданный случайными числами на промежутке [-100; 100). Вывести на экран исходный и отсортированный массивы.

---
+++ task7_1.py
@@ -0,0 +1,27 @@
+"""
+1. Отсортировать по убыванию методом «пузырька» одномерный целочисленный массив,
+заданный случайными числами на промежутке [-100; 100).
+Вывести на экран исходный и отсортированный массивы.
+"""
+
+def bubble_sort(array):
+
+    if array == sorted(array, reverse=True):
+        return array
+
+    n = len(array)
+
+    while n > 0:
+        for i in range(1, n):
+            if array[-i] > array[-(i + 1)]:
+                array[-i], array[-(i + 1)] = array[-(i + 1)], array[-i]
+
+        n -= 1
+    return array
+
+import random
+
+array = [random.randint(-100, 100) for _ in range(10)]
+
+print(f'source: {array}')
+print(f'sorted: {bubble_sort(array[:])}')
